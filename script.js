df9f60dc5cab5afc7b155737fb71c2302ff73a52
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-05-01 22:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  alert(`${firstName}+" "+${lastName}`);
 }
+
