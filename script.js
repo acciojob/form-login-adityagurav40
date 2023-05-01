4292db2e7d2afb7302b5293b32d9849d614eb942
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-05-01 22:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getFormValue() {
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
-  alert(`First name: ${firstName}\nLast name: ${lastName}`);
+  alert(`${firstName}+" " +${lastName}`);
 }
